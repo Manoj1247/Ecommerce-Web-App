817de84780bc31e0cfd8cfbf09cd07ff8afc3a35
Manoj Kalaganti <79302349+Manoj1247@users.noreply.github.com>
2023-07-13 23:01:40 -0700

    First Commit

---
+++ Ecommerce/app.js
@@ -0,0 +1,65 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+const mongodb = require('mongodb');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findOne()
+    .then(user => {
+      console.log(user, 'userr');
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    'mongodb+srv://manojkalaganti:Isrop1247*@cluster0.dpn4clj.mongodb.net/',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      console.log(user)
+      if (!user) {
+        //console.log('hii');
+        const user = new User({
+          name: 'Manoj',
+          email: 'manojsaikalaganti@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
